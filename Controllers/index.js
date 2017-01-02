88a8fcd4b67fa302c2f1736033c438a3e34ee8a4
Antoine Dcr <alex.gamegame@laposte.net>
2017-01-02 23:14:59 +0100

    MAJ Importante
    Réorganisation des noms de fichiers, variables...
    Création du layout de base et mise en place du routage
    Réecriture du carousel
    Ajout d'icones css (bootstrap + fontawesome)

--- Controllers/index.js
+++ Controllers/index.js
@@ -1,7 +1,40 @@
-var app = angular.module('app', ['ui.bootstrap', 'ngTouch', 'ngAnimate']);
+//Déclaration de notre application, ainsi que ses dépendances aux autres modules, nommée
+//puzzleProjectApp
+var puzzleProjectApp = angular.module('puzzleProjectApp',
+	[
+		'ui.bootstrap', 	//Angular-ui-bootstrap
+		'ngTouch',
+		'ngAnimate',
+		'ngRoute',
+		'ngSanitize',
+		'ngResource',
+		'appControllers'	//Module contenant nos controlleurs
+	]
+);
 
-app.controller('ctrl', function($scope, $interval, $http){
+//Configuration de l'application
+puzzleProjectApp.config(['$routeProvider',
+    function($routeProvider) {
+
+        //Configuration du système de routage
+        $routeProvider
+				.when('/home',	//Pour href="#/home"
+				{
+            templateUrl: 'home.html',	//Template de la page à afficher
+            controller: 'homeCtrl'	//Controleur correspondant au template
+        })
+        .otherwise({
+          redirectTo: '/home'		//Chemin par défaut
+        })
+    }
+]);
+
+//Déclaration du module contenant nos controleurs
+var appControllers = angular.module('appControllers', []);
+
+/*app.controller('ctrl', function($scope, $interval, $http){
 	$scope.json = function () {
 		return "toto";
 	}
-});
+
+});*/
