1b0748970dfc9e34a522c2228ef54db0d10a9e0d
Alex <alexandre.rosset97@laposte.net>
2017-01-22 13:46:54 +0100

    about&contact
    rédaction des pages about et contact

--- Controllers/home.js
+++ Controllers/home.js
@@ -1,9 +1,9 @@
 
 appControllers.controller('homeCtrl', function($scope, $interval, $http){
   $scope.myInterval = 30000;
-  $scope.listImages = [{"img":"../couleur-rose-5.jpg", "id": 0},
-  {"img":"../couleur-rose-6.jpg", "id": 1},
-  {"img":"../couleur-rose-7.png", "id": 2},
-  {"img":"../couleur-rose-8.jpg", "id": 3}];
+  $scope.listImages = [{"img":"../15966832_780309965460156_1698823445_o.jpg", "id": 0},
+  {"img":"../15966832_780309965460156_1698823445_o.jpg", "id": 1},
+  {"img":"../15966832_780309965460156_1698823445_o.jpg", "id": 2},
+  {"img":"../15966832_780309965460156_1698823445_o.jpg", "id": 3}];
 
 });
