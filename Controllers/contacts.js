985254f8ca580523061981db7d809097f209512e
Antoine Dcr <alex.gamegame@laposte.net>
2017-01-25 03:02:22 +0100

    Paufinage
    Dernières corrections

--- Controllers/contacts.js
+++ Controllers/contacts.js
@@ -1,5 +1,9 @@
 appControllers.controller('contactCtrl', function($scope, $sce){
 
+  $scope.sendMessage = function(contactform){
+    
+  };
+
   $scope.valideURL = function(src){
     return $sce.trustAsRessourceUrl(src);
   }
